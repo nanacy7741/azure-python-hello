977519e9b9e27140e2a61ba51d19e792655974f8
Uchida Yosuke <yosuke.uchida@ntt.local>
2020-02-19 11:36:45 +0900

    a

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def hello():
+    name = "Hoge"
+    #return name
+    return render_template('index.html', title='flask test', name=name)
 
-if __name__ == '__main__':
-  app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
